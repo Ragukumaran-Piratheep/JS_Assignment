0944e60a452bbcb9b684c3b4a63202aca2781982
Ragukumaran Piratheep <rpiratheep2002@gmail.com>
2023-11-09 21:11:28 +0530

    added

---
+++ Assets/Js/script.js
@@ -0,0 +1,9 @@
+let icon = document.getElementById("icon");
+
+icon.onclick = function() {
+    document.body.classList.contains("dark-theme");
+
+    if (document.body.classList.contains("dark-theme")) {
+        
+    }
+}
